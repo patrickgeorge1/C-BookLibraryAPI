f77c1bc4f693b87f8c43be16dfe02128a8bf7603
patrickgeorge1 <patrionpatrick@gmail.com>
2020-05-07 21:30:55 +0300

    finisher TG

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
 #include "Utils/utils.h"
 #include "Utils/api.h"
-#include <stdio.h>      /* printf, sprintf */
-#include <stdlib.h>     /* exit, atoi, malloc, free */
-#include <unistd.h>     /* read, write, close */
-#include <string.h>     /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h>      /* struct hostent, gethostbyname */
-#include <arpa/inet.h>
+#include <stdlib.h>
+
 
 
 using namespace std;
